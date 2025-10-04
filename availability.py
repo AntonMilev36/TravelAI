c9b38e12cd91fe4520aa7a3e3d0d35b6a048d9fe
AntonMilev36 <milev.anton36@gmail.com>
2025-10-04 13:41:06 +0300

    Adding database memory

--- availability.py
+++ availability.py
@@ -13,29 +13,50 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import Runnable
 from langchain_core.tools import Tool
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
+from langgraph.checkpoint.postgres import PostgresSaver
 from langgraph.prebuilt import create_react_agent
-from langgraph.checkpoint.memory import InMemorySaver
+from psycopg_pool import ConnectionPool
 
 load_dotenv()
 
-memory = InMemorySaver()
 LLM_MODEL = "gemini-2.5-flash"
 EMBEDDING_MODEL = "gemini-embedding-001"
 BUS_STOPS_URL = ("https://pirdop.wordpress.com/%D1%80%D0%B0%D0%B7%D0%BF%D0%B8%D1%81%D0%B0%D0%BD%D0%B8%D0%B5"
                  "-%D0%BD%D0%B0-%D0%B0%D0%B2%D1%82%D0%BE%D0%B1%D1%83%D1%81%D0%B8/")
+
+
+def create_database():
+    pg_user = os.getenv("PG_USER")
+    pg_password = os.getenv("PG_PASS")
+    pg_host = os.getenv("PG_HOST")
+    pg_port = os.getenv("PG_PORT")
+    db_name = os.getenv("DB_NAME")
+
+    pool = ConnectionPool(
+        conninfo=f"postgresql://{pg_user}:{pg_password}@{pg_host}:{pg_port}/{db_name}",
+        max_size=20,
+        kwargs={"autocommit": True}
+    )
+
+    # The pool provides required information. Ignore this warning
+    checkpointer = PostgresSaver(pool)
+
+    return checkpointer, pool
+
 
 def get_web_docs(url):
     loader = WebBaseLoader(url)
     docs = loader.load()
 
     splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000,
-        chunk_overlap=100
+        chunk_size=2000,
+        chunk_overlap=250
     )
 
     split_docs = splitter.split_documents(docs)
 
     return split_docs
+
 
 def define_model():
     llm = ChatGoogleGenerativeAI(
@@ -53,6 +74,7 @@ def define_model():
 
     return llm, prompt
 
+
 def create_chain(llm, prompt):
     chain = create_stuff_documents_chain(
         llm=llm,
@@ -60,6 +82,7 @@ def create_chain(llm, prompt):
     )
 
     return chain
+
 
 def create_vector_db(docs):
     embedding = GoogleGenerativeAIEmbeddings(
@@ -73,6 +96,7 @@ def create_vector_db(docs):
     )
 
     return vector_store
+
 
 def create_retriever(llm, prompt, chain, vector_store):
     retriever = vector_store.as_retriever()
@@ -89,8 +113,14 @@ def create_retriever(llm, prompt, chain, vector_store):
 
     return retrieval_chain
 
+
 def define_tool_agent(retrieval_chain, llm):
+    memory, pool = create_database()
+    memory.setup()
+
+    # Here the retriever is runed in a function so we can pass it to the Tool func kwarg
     def run_retrieval(query: str | dict) -> str:
+        # Checking if model is using the tool, when giving answer
         print("Debug retriever runner: ", query)
         if isinstance(query, dict):
             agent_input = query.get("__arg1") or query.get("input") or str(query)
@@ -107,13 +137,14 @@ def define_tool_agent(retrieval_chain, llm):
                     "bus and train schedules, stops, and times from the uploaded documents."
     )
 
-    model_agent = create_react_agent(
+    agent = create_react_agent(
         model=llm,
         tools=[retrieval_tool],
         checkpointer=memory
     )
 
-    return model_agent
+    return agent, pool
+
 
 if __name__ == "__main__":
     urls = get_web_docs(BUS_STOPS_URL)
@@ -121,26 +152,28 @@ if __name__ == "__main__":
     model_chain = create_chain(model, model_prompt)
     vector_db = create_vector_db(urls)
     model_retriever = create_retriever(model, model_prompt, model_chain, vector_db)
-    agent = define_tool_agent(model_retriever, model)
-    thread_id = str(uuid.uuid4())
+    model_agent, model_pool = define_tool_agent(model_retriever, model)
+    thread_id: str = str(uuid.uuid4())
+    previous_chat: str | None = input("Enter thread id, if you want new chat, press Enter: ")
 
     while True:
         user_input = input("Your message: ")
 
         if user_input == "exit":
+            model_pool.close()
             break
 
         # This ensures model memory in a sertan conversation
-        config = {"configurable": {"thread_id": thread_id}}
+        config: dict[str, dict] = {"configurable": {"thread_id": previous_chat if not None else thread_id}}
 
-        # By documentation the agent has to be runed like this. Ignore the warning
-        response = agent.invoke(
+        # By documentation the agent has to be runed like this. Ignore this warning
+        response = model_agent.invoke(
             {
                 "messages": [{"role": "user", "content": user_input}]
             }, config
         )
 
-        # Getting only the last answer from the agent
+        # Getting only the answer from the agent
         messages = response.get("messages", [])
 
         last_ai_message = next(
