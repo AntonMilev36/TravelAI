fa8530590d252c360e8677cc776212d0ed437fb1
AntonMilev36 <milev.anton36@gmail.com>
2025-10-06 18:59:34 +0300

    Saveing docs in database

--- availability.py
+++ availability.py
@@ -1,5 +1,7 @@
+import datetime
 import os
 import uuid
+from datetime import timezone
 from typing import Any
 
 from dotenv import load_dotenv
@@ -20,24 +22,41 @@ from langgraph.checkpoint.postgres import PostgresSaver
 from langgraph.graph.state import CompiledStateGraph
 from langgraph.prebuilt import create_react_agent
 from psycopg_pool import ConnectionPool
+from sqlalchemy import Column, String, Text, JSON, TIMESTAMP, create_engine
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 load_dotenv()
 
+PG_USER: str = os.getenv("PG_USER")
+PG_PASSWORD: str = os.getenv("PG_PASS")
+PG_HOST: str = os.getenv("PG_HOST")
+PG_PORT: str = os.getenv("PG_PORT")
+DB_NAME: str = os.getenv("DB_NAME")
+
 LLM_MODEL: str = "gemini-2.5-flash"
 EMBEDDING_MODEL: str = "gemini-embedding-001"
+DB_URI: str = f"postgresql://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{DB_NAME}"
 BUS_STOPS_URL: str = ("https://pirdop.wordpress.com/%D1%80%D0%B0%D0%B7%D0%BF%D0%B8%D1%81%D0%B0%D0%BD%D0%B8%D0%B5"
-                 "-%D0%BD%D0%B0-%D0%B0%D0%B2%D1%82%D0%BE%D0%B1%D1%83%D1%81%D0%B8/")
+                      "-%D0%BD%D0%B0-%D0%B0%D0%B2%D1%82%D0%BE%D0%B1%D1%83%D1%81%D0%B8/")
+
+Base = declarative_base()
+
+
+class Documents(Base):
+    __tablename__ = "documents"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    source_type = Column(String, nullable=False)  # 'pdf', 'url', 'note', etc.
+    source = Column(String, nullable=False)  # path or URL
+    content = Column(Text, nullable=False)
+    meta = Column(JSON, nullable=True)
+    created_at = Column(TIMESTAMP, default=datetime.datetime.now(timezone.utc))
 
 
 def create_database():
-    pg_user: str = os.getenv("PG_USER")
-    pg_password: str = os.getenv("PG_PASS")
-    pg_host: str = os.getenv("PG_HOST")
-    pg_port: str = os.getenv("PG_PORT")
-    db_name: str = os.getenv("DB_NAME")
-
     pool = ConnectionPool(
-        conninfo=f"postgresql://{pg_user}:{pg_password}@{pg_host}:{pg_port}/{db_name}",
+        conninfo=DB_URI,
         max_size=20,
         kwargs={"autocommit": True}
     )
@@ -50,16 +69,24 @@ def create_database():
 
 def get_web_docs(url):
     loader: WebBaseLoader = WebBaseLoader(url)
-    docs: list[Document] = loader.load()
+    documents: list[Document] = loader.load()
 
-    splitter = RecursiveCharacterTextSplitter(
-        chunk_size=2000,
-        chunk_overlap=250
-    )
+    return documents
 
-    split_docs: list[Document] = splitter.split_documents(docs)
 
-    return split_docs
+def save_docs(documents: list[Document]):
+    engine = create_engine(DB_URI)
+    Base.metadata.create_all(engine)
+    session_maker = sessionmaker(bind=engine)
+    session = session_maker()
+
+    for d in documents:
+        text = session.query(Documents).filter(Documents.source == d.metadata["source"]).first()
+        if not text:
+            doc = Documents(source_type="url", source=d.metadata["source"], content=d.page_content)
+            session.add(doc)
+
+    session.commit()
 
 
 def define_model():
@@ -88,16 +115,32 @@ def create_chain(llm: ChatGoogleGenerativeAI, prompt: ChatPromptTemplate):
     return chain
 
 
-def create_vector_db(docs: list[Document]):
+def create_vector_db(documents):
     embedding = GoogleGenerativeAIEmbeddings(
         model=EMBEDDING_MODEL,
         google_api_key=os.getenv("GOOGLE_API_KEY")
     )
 
+    chunk_ids = []
+    chunked_docs = []
+
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=2000,
+        chunk_overlap=250
+    )
+
+    for document in documents:
+        split_docs: list[Document] = splitter.split_documents([document])
+        for i, chunk in enumerate(split_docs):
+            chunk_id = f"{document.id}-{i}"
+            chunk_ids.append(chunk_id)
+            chunked_docs.append(chunk)
+
     vector_store = Chroma.from_documents(
         embedding=embedding,
-        documents=docs,
+        documents=chunked_docs,
         persist_directory=os.getenv("CHROMA_PATH"),
+        ids=chunk_ids
     )
 
     return vector_store
@@ -160,14 +203,16 @@ if __name__ == "__main__":
     model_agent: CompiledStateGraph
     model_pool: ConnectionPool
 
-    urls: list[Document] = get_web_docs(BUS_STOPS_URL)
+    docs = get_web_docs(BUS_STOPS_URL)
     model, model_prompt = define_model()
     model_chain: Runnable = create_chain(model, model_prompt)
-    vector_db: Chroma = create_vector_db(urls)
+    vector_db: Chroma = create_vector_db(docs)
     model_retriever: Runnable = create_retriever(model, model_prompt, model_chain, vector_db)
     model_agent, model_pool = define_tool_agent(model_retriever, model)
     thread_id: str = str(uuid.uuid4())
     previous_chat: str | None = input("Enter thread id, if you want new chat, press Enter: ")
+
+    save_docs(docs)
 
     while True:
         user_input: str = input("Your message: ")
