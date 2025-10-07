d34952dce51803befab035150a8c7247b54a0f3f
AntonMilev36 <milev.anton36@gmail.com>
2025-10-07 17:46:04 +0300

    Adding script to get train schedule

---
+++ script.py
@@ -0,0 +1,50 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+options = Options()
+
+driver = webdriver.Chrome(options=options)
+
+def get_train_url():
+    try:
+        driver.get("https://www.bdz.bg/bg")
+
+        cookie_button = WebDriverWait(driver, 10).until(
+            expected_conditions.element_to_be_clickable(
+                (
+                    By.CSS_SELECTOR,
+                    ".btn.btn-primary.btn-sm.mr-1.cw-close",
+                )
+            )
+        )
+        cookie_button.click()
+
+        from_station = driver.find_element(By.NAME, "from")
+        to_station = driver.find_element(By.NAME, "to")
+
+        from_station.clear()
+        from_station.send_keys("ПИРДОП")
+        to_station.send_keys("БУРГАС")
+
+        button = WebDriverWait(driver, 10).until(
+            expected_conditions.element_to_be_clickable((By.CSS_SELECTOR, ".btn.btn-primary.btn-sm.px-3.search-submit"))
+        )
+        button.click()
+
+        time.sleep(3)
+
+        change_button = driver.find_elements(By.CSS_SELECTOR, ".text-danger.dropdown-toggle")
+        for el in change_button:
+            try:
+                driver.execute_script("arguments[0].scrollIntoView(true);", el)
+                driver.execute_script("arguments[0].click();", el)
+            except Exception as e:
+                print("Mission kaput")
+    finally:
+        driver.close()
+        return driver.current_url
